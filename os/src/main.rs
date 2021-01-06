ee990a2f5f08d3a7f0700e4e1689ae700c99cc37
JiangBo <jiangbo88999@gmail.com>
2021-01-06 20:31:22 +0800

    调整内存布局：修改链接脚本

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+//! # 全局属性
+//! - `#![no_std]`
+//! 禁用标准库
+#![no_std]
+
+//!
+//! - `#![no_main]`
+//! 不使用`main`函数等全部 Rust-level 入口点作为程序入口
+#![no_main]
+
+use core::panic::PanicInfo;
+
+// 这个函数在panic时被调用
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
+
+// 覆盖 crt_0 中的 _start 函数
+#[no_mangle] // 不重整函数名
+pub extern "C" fn _start() -> ! {
+    // 编译器会寻找一个名为`_start`的函数， 所以这个函数就是入口点
+    // 默认命名为`_start`
+    loop {}
+}
+
+
