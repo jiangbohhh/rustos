6c44f3523d2341ff3a7f7be190bca018fac89261
JiangBo <jiangbo88999@gmail.com>
2021-01-09 22:13:12 +0800

    实现中断上下文环境保存与恢复

--- os/src/interrupt.rs
+++ os/src/interrupt.rs
@@ -5,16 +5,27 @@ use riscv::register:: {
     sscratch
 };
 
+use crate::context::TrapFrame;
+
+global_asm!(include_str!("trap/trap.asm"));
+
+
 pub fn init() {
     unsafe {
+        extern "C" {
+            // 中断处理总入口
+            fn __alltraps();
+        }
+        // 由于是内核态 需要把 sscratch
         sscratch::write(0);
-        stvec::write(trap_handler as usize, stvec::TrapMode::Direct);
+        stvec::write(__alltraps as usize, stvec::TrapMode::Direct);
     }
+    println!("++++ interrupt setup ++++");
 }
 
-fn trap_handler() -> ! {
-    let cause = scause::read().cause();
-    let epc = sepc::read();
-    println!("trap: cause: {:?}, epc: 0x{:#x}", cause, epc);
-    panic!("trap handler!");
+#[no_mangle]
+pub fn rust_trap(tf: &mut TrapFrame) {
+    println!("rust_trap!");
+    tf.sepc += 2;
 }
+
